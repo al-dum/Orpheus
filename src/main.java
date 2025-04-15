f3181ffb900889fd4d9d10d38d25ebf4eaae11e1
Aldo <aldod.zamoral@icloud.com>
2025-04-15 15:10:03 +0200

    test 1

--- src/main.java
+++ src/main.java
@@ -2,8 +2,4 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
-
-    public static void test() {
-        System.out.println("Test method");
-    }
 }
