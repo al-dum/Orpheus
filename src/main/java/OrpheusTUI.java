f89e16af138723f758720b3139494e7e43f1f024
Aldo <aldod.zamoral@icloud.com>
2025-05-12 16:42:03 +0200

    Add GUI components to OrpheusTUI and clean up pom.xml formatting

--- src/main/java/OrpheusTUI.java
+++ src/main/java/OrpheusTUI.java
@@ -1,8 +1,10 @@
 import com.google.gson.JsonObject;
+import java.awt.*;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
